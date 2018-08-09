677045c53910621501dbaf555fd13676aeb0f715
meltifa <meltifa@gmail.com>
2018-08-09 18:59:59 +0800

    hello world

--- src/controller/base.js
+++ src/controller/base.js
@@ -3,10 +3,9 @@ module.exports = class extends think.Controller {
     const userInfo = await this.session('userInfo');
     if (!think.isEmpty(userInfo)) {
       this.assign('userInfo', userInfo);
-      const priv = await this.model('priv').select();
-      const curPriv = priv.find(item => item.name === this.ctx.url);
-      if (curPriv) {
-        if (userInfo.priv.some(item => item === curPriv.id)) {
+      const privId = await this.model('priv').where({ name: this.ctx.url }).getField('id', true);
+      if (!think.isEmpty(privId)) {
+        if (userInfo.priv.some(item => item === privId)) {
           return this.success(null, '你有权限访问本页');
         } else {
           return this.fail(403, '你没有权限访问本页');
